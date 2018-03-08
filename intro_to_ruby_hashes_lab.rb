6cbfce2fa462085504963c988281ed1683f575c8
Mark Cruz <markchriscruz@yahoo.com>
2018-03-08 01:42:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,8 @@ def monopoly_with_fourth_tier
 	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 	monopoly[:railroads][:names][:b_and_o_railroad] = {}
 	monopoly[:railroads][:names][:shortline] = {}
-	monopoly[:railroads][:names][:reading_railroad][""]
+	monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = 100
+	return monopoly
 end
 
 
